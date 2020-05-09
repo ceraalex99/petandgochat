69164e8cb3c25af168799c84b8aa8954a1c35395
alex.cera <alex.cera@est.fib.upc.edu>
2020-05-09 20:30:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
+import json
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+clients = {}
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+@socketio.on('message')
+def handleMessage(payloadJson):
+    payload = json.loads(payloadJson)
+    requests.post('https://petandgo.herokuapp.com/api/mensajes', data=payloadJson)
+    emit('newMsg', payloadJson, room=clients[payload['receiver']])
+
+
+@socketio.on('join')
+def join(email):
+    clients[email] = request.sid
+
+
+
+if __name__ == '__main__':
+    socketio.run(app)
