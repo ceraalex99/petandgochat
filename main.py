00beca270727bf5d51982c3469b884bd674ef386
ceraalex99 <ceraalex99@gmail.com>
2020-05-12 18:25:20 +0200

    no namespace

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 import json
+import logging
 import requests
+
+logging.basicConfig(level=logging.NOTSET)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -25,7 +28,7 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('message', namespace='/chat')
+@socketio.on('message')
 def handle_message(payload_json):
     payload = json.loads(payload_json)
     # requests.post('https://petandgo.herokuapp.com/api/mensajes', data=payload_json)
@@ -38,40 +41,43 @@ def handle_message(payload_json):
     #     }
     #     result = requests.post(fcmURL, headers=headers, data=json.dumps(data_raw))
     #     print(result)
+    print(payload_json)
     emit('message', payload_json)
 
-@socketio.on('join', namespace='/chat')
+@socketio.on('join')
 def join(email):
     clients[email] = request.sid
     emit('joined', {'data': 'Lets dance'})
 
 
-@socketio.on('connect', namespace='/chat')
+@socketio.on('connect')
 def test_connect():
+    print('connected')
     clients_test.append(request.sid)
     emit('connect', {'data': 'Connected'})
 
 
-@socketio.on('disconnect', namespace='/chat')
+@socketio.on('disconnect')
 def test_disconnect():
     clients_test.remove(request.sid)
     clients.pop(list(clients.keys())[list(clients.values()).index(request.sid)])
 
 
-@socketio.on('my_event', namespace='/chat')
+@socketio.on('my_event')
 def test_message(message):
     emit('my_response', {'data': message['data']}, room=clients_test[0])
 
 
-@socketio.on('my_event2', namespace='/chat')
+@socketio.on('my_event2')
 def test_message3(message):
     emit('my_response', {'data': message['data']}, room=clients_test[1])
 
 
-@socketio.on('my_broadcast_event', namespace='/chat')
+@socketio.on('my_broadcast_event')
 def test_message2(message):
-    emit('my_response', {'data': message['data']}, broadcast=True)
+    emit('message', {'data': message['data']}, broadcast=True)
 
 
 if __name__ == '__main__':
+    print("hola")
     socketio.run(app)
